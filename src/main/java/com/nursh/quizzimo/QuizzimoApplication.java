e2313974b8b5076062471f8efa5c55e787418d4b
nursh <nsheikhmohammed@yahoo.com>
2017-11-11 18:28:20 -0500

    Initial commit

---
+++ src/main/java/com/nursh/quizzimo/QuizzimoApplication.java
@@ -0,0 +1,12 @@
+package com.nursh.quizzimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzimoApplication.class, args);
+	}
+}
