ece39af7fdcf8a6f2923556e7cb710561533b7e0
nursh <nsheikhmohammed@yahoo.com>
2017-11-11 22:39:55 -0500

    Finished Babel configuration with eslint for JS files and configuring html with Spring Controller

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+const PATHS = {
+	src: path.resolve(__dirname, 'src/main/JS'),
+	build: path.resolve(__dirname, 'src/main/resources/static/built'),
+};
+
+module.exports = {
+	context: PATHS.src,
+	entry: {
+		app: './index.jsx',
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{ test: /\.jsx/, loader: 'babel-loader' }
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			filename: '[name].js',
+			minChunks(module) {
+				return module.context && module.context.indexOf('node_modules') !== -1;
+			},
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'manifest',
+			minChunks: Infinity,
+		}),
+	],
+};
