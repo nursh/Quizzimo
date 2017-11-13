2eec259895920039c86750696bdd21388018f6d9
nursh <nsheikhmohammed@yahoo.com>
2017-11-13 08:02:44 -0500

    Added Basic Unit Tests for Category and Question class

---
+++ src/test/java/com/nursh/quizzimo/Category/CategoryTest.java
@@ -0,0 +1,14 @@
+package com.nursh.quizzimo.Category;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+}
---
+++ src/test/java/com/nursh/quizzimo/Question/QuestionTest.java
@@ -0,0 +1,34 @@
+package com.nursh.quizzimo.Question;
+
+import org.junit.jupiter.api.Test;
+
+class QuestionTest {
+    @Test
+    void getQuestion() {
+    }
+
+    @Test
+    void getAnswer() {
+    }
+
+    @Test
+    void getOptions() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setQuestion() {
+    }
+
+    @Test
+    void setAnswer() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+}
