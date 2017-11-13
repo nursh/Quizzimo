ef88201e7bf3d08cbd460054a15a6b145a2cfd08
nursh <nsheikhmohammed@yahoo.com>
2017-11-13 15:53:00 -0500

    Added unit test for the Category Entity

--- src/test/java/com/nursh/quizzimo/Category/CategoryTest.java
+++ src/test/java/com/nursh/quizzimo/Category/CategoryTest.java
@@ -2,13 +2,21 @@ package com.nursh.quizzimo.Category;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CategoryTest {
+
+    static Category ct = new Category("Languages");
+
     @Test
     void getName() {
+        assertEquals("Languages", ct.getName());
     }
 
     @Test
     void setName() {
+        ct.setName("Witchcraft");
+        assertEquals("Witchcraft", ct.getName());
     }
 
 }
